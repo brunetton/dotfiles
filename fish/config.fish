f08bbc3e9ba49b03ae240892f253c965ddf76c0c
Bruno DuyÃ© <brunetton@gmail.com>
2017-09-27 15:35:48 +0200

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 # Workaround to disable copy on delete (https://github.com/fish-shell/fish-shell/issues/772)
 set FISH_CLIPBOARD_CMD "cat"
 
+set PATH $PATH ~/bin/ ~/bin/bin/
+
 alias s="cd .."
 alias nemo="nemo ."
 alias atom="atom ."
