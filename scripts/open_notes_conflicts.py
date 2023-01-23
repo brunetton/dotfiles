a85ec77c6aef1563d1c8f56f00e771c742083b17
Bruno Duyé <brunetton@brunetton.org>
2023-01-23 13:18:26 +0100

    Add resolve notes conflicts script

---
+++ scripts/open_notes_conflicts.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import os
+import re
+from pathlib import Path
+
+NOTES_PATH = "/home/bruno/Notes/"
+
+notes_path = Path(NOTES_PATH)
+
+# List conflicts
+conflicts_paths = list(notes_path.glob('**/*sync-conflict*.md'))
+
+for conflict_path in conflicts_paths:
+    print(f"-> {conflict_path}")
+
+    # remove ".sync-conflict-20230120-155918-UPE55MT" from path
+    match = re.search(r'(.*)\.sync-conflict.*\.md', str(conflict_path))
+    assert(match)
+    original_path = Path(match.group(1) + '.md') # ex: /home/bruno/Notes/Divers/poker.md
+
+    # open in Meld
+    os.system(f"meld \"{original_path}\" \"{conflict_path}\"")
+
+    # Move to trash
+    os.system(f"trash-put \"{conflict_path}\"")  # apt install trash-cli
+
+print("END")
