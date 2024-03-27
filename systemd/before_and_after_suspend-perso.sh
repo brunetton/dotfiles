9d5f5b9e3bfb42cb7cba8984df2458321a2b08b3
Bruno Duyé <brunetton@brunetton.org>
2024-03-27 11:48:41 +0100

    Fix: systemd suspend scripts

--- systemd/before_and_after_suspend-perso.sh
+++ systemd/before_and_after_suspend-perso.sh
@@ -4,6 +4,8 @@ case $1 in
     # Umount Google drive
     umount -l '/home/bruno/Google drive'
     killall google-drive-ocamlfuse -9
+    killall kioslave5 -9
+    umount /home/bruno/sshfs/naq
   ;;
   post)
     # # Restart xbanish
